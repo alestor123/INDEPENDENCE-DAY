d894f9c6b7d093442ab282708cca316edbff1690
alestor123 <alestoraldous@gmail.com>
2021-01-07 17:11:50 +0530

    added main code

---
+++ App.js
@@ -0,0 +1,10 @@
+var chalk = require('chalk'),
+figlet = require('figlet'),
+log = require('log-update');
+setInterval(() =>{
+figlet('happy independence day'.toUpperCase(), (err, data) => {
+var frames = [chalk.red(data),chalk.green(data),chalk.blue(data),chalk.magenta(data)]
+if (err) throw err
+log(`\n ${frames[Math.floor(Math.random() * frames.length)] } \n`)
+});
+},100);
